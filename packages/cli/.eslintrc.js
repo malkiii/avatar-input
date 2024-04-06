1b95dc6558282c2613ec982587bf3593afe92e37
malkiAbdoo <abdo.malkiep@gmail.com>
2024-04-06 23:55:10 +0100

    chore: update the ESLint configs

--- packages/cli/.eslintrc.js
+++ packages/cli/.eslintrc.js
@@ -1,10 +1,14 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ['@repo/eslint-config/next.js'],
+  extends: ['@repo/eslint-config/react-internal.js'],
   parser: '@typescript-eslint/parser',
-  parserOptions: { project: './tsconfig.json' },
+  parserOptions: {
+    project: './tsconfig.lint.json',
+    tsconfigRootDir: __dirname,
+  },
   rules: {
+    'no-undef': 'off',
     'no-unused-vars': 'off',
     '@next/next/no-img-element': 'off',
   },
