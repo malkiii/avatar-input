30c3af8bb4100da6566e737dfc938830c360310d
malkiAbdoo <abdo.malkiep@gmail.com>
2024-04-06 22:52:14 +0100

    ci: enable static exports

--- apps/demo/next.config.cjs
+++ apps/demo/next.config.cjs
@@ -2,6 +2,7 @@ const { homepage } = require('../../packages/cli/package.json');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   basePath: new URL(homepage).pathname,
 };
